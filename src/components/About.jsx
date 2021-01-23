dedb08d8f1a04b7fb78060bd67273ee86b064525
RuddraNickBiswas <biswasruddra100@gmail.com>
2021-01-23 15:30:47 +0600

    new-2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-        Hahahahah Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis illum, ducimus in natus molestiae vero, dicta quos ratione ipsam iure consectetur sed, tenetur magnam blanditiis quod corporis mollitia. Laborum, dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil veritatis laborum velit quibusdam facilis numquam architecto eaque, natus quo amet quia consequuntur animi pariatur. Quisquam sunt similique non numquam soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur cumque iure corporis tempore dolorum eos magnam ad dolor blanditiis delectus minima exercitationem incidunt distinctio voluptates consequatur rem laborum, sequi enim. lore, 
+            Hahahahah Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis illum, ducimus in natus molestiae vero, dicta quos ratione ipsam iure consectetur sed, tenetur magnam blanditiis quod corporis mollitia. Laborum, dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil veritatis laborum velit quibusdam facilis numquam architecto eaque, natus quo amet quia consequuntur animi pariatur. Quisquam sunt similique non numquam soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur cumque iure corporis tempore dolorum eos magnam ad dolor blanditiis delectus minima exercitationem incidunt distinctio voluptates consequatur rem laborum, sequi enim. lore, 
         </div>
     )
 }
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { AppBar, Button, Menu, MenuItem, Tab, Tabs, Toolbar } from "@material-ui/core";
+import { AppBar, Button, IconButton, List, ListItem, ListItemText, Menu, MenuItem, SwipeableDrawer, Tab, Tabs, Toolbar, useMediaQuery, useTheme } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,15 +18,39 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: 50,
     borderRadius: 0,
   },
+  menu: {
+    backgroundColor: theme.palette.primary.main,
+    color: "white",
+    borderRadius: "0px"
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    "&:hover": {
+      opacity: 1
+    }
+  },
+  drawer :{
+    padding :100
+  },
+
 }));
 
 export default function Header() {
   const [routeValue, setRouteValue] = React.useState(0);
   const [activeIndex, setActiveIndex] = React.useState();
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [openDrawer, setOpenDrawer] = React.useState(false);
   const [openMenu, setOpenMenu] = React.useState(false);
   const [selectedIndex, setSelectedIndex] = React.useState(false);
+
+  const theme = useTheme()
+
+  const matches = useMediaQuery(theme.breakpoints.down('md'))
+
   const classes = useStyles();
+
+
 
 
   const handleRouteChange = (e, newValue) => {
@@ -75,14 +99,13 @@ export default function Header() {
       link: "/websites",
       activeIndex: 1,
       selectedIndex: 3
-    }
+    },
+   
   ];
 
-  return (
+  const tabs =(
     <>
-      <AppBar>
-        <Toolbar disableGutters>
-          <Tabs
+      <Tabs
             className={classes.tab}
             value={routeValue}
             onChange={handleRouteChange}
@@ -107,11 +130,13 @@ export default function Header() {
             MenuListProps = {{
               onMouseLeave : handleMenuClose
             }}
+            classes = {{paper :classes.menu}}
             elevation = {0}
             >
               {menuOptions.map((option, i) => (
                 <MenuItem
                 key={`${option}${i}`}
+                className = {classes.menuItem}
                 component = {Link}
                 to = {option.link}
                 onClick = {event => {
@@ -126,6 +151,58 @@ export default function Header() {
               ))}
             </Menu>
           </Tabs>
+    </>
+  )
+  const drawer = (
+    <>
+    <SwipeableDrawer
+    open ={openDrawer}
+    onOpen = {() => setOpenDrawer(true)}
+    onClose = { () => setOpenDrawer(false)}
+    classes = {{paper : classes.drawerPaper}}
+    className = {classes.drawer}
+    >
+      <div className = {classes.toolbarMargin}></div>
+     <List disablePadding
+     
+     >
+       {routes.map((route , i) => (
+          <ListItem
+          key = {`${route}`}
+          divider
+          button
+          component = {Link}
+          
+          to = {route.link}
+          onClick = {() => {setOpenDrawer(false); setRouteValue(route.activeIndex)}}
+         
+          >
+          <ListItemText disableTypography>
+           {route.name}
+          </ListItemText>
+          </ListItem>
+       ))}
+     
+     </List>
+     
+    </SwipeableDrawer>
+      <IconButton
+      style = {{marginLeft : "auto"}}
+      className={classes.drawerIconContainer}
+      onClick={() => setOpenDrawer(!openDrawer)}
+      disableRipple
+    >
+     X
+    </IconButton>
+    </>
+  )
+
+  return (
+    <>
+      <AppBar>
+        <Toolbar disableGutters>
+      {matches ? drawer : tabs}
+       
         </Toolbar>
       </AppBar>
       <div className={classes.toolbarMargin} />
